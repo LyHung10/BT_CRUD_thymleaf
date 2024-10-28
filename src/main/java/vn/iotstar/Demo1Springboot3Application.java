4ab745308a710af050e88dc9f27c66f966342d82
LyHung10 <lyhung10nctlop95@gmail.com>
2024-10-28 16:58:35 +0700

    update cRUD

--- src/main/java/vn/iotstar/Demo1Springboot3Application.java
+++ src/main/java/vn/iotstar/Demo1Springboot3Application.java
@@ -2,10 +2,6 @@ package vn.iotstar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-
-import vn.iotstar.configs.CustomSiteMeshFilter;
 
 @SpringBootApplication
 public class Demo1Springboot3Application {
@@ -14,14 +10,5 @@ public class Demo1Springboot3Application {
 		SpringApplication.run(Demo1Springboot3Application.class, args);
 	}
 	
-	 @Bean
-	    public FilterRegistrationBean<CustomSiteMeshFilter> siteMeshFilter() {
-	        FilterRegistrationBean<CustomSiteMeshFilter> filterRegistrationBean = new FilterRegistrationBean<>();
-	        
-	        filterRegistrationBean.setFilter(new CustomSiteMeshFilter()); // Thêm bộ lọc SiteMesh tùy chỉnh
-	        filterRegistrationBean.addUrlPatterns("/*"); // Áp dụng cho tất cả các URL
-	        
-	        return filterRegistrationBean;
-	    }
 
 }
